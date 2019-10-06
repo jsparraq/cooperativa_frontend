7e167d053caaf64023fb4cb57cb1942a1c2a270f
Sebastian Parra <sebastian.parra96@gmail.com>
2019-10-06 18:55:32 -0500

    Added webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line
 import App from "./components/App";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 // If you want your app to work offline and load faster, you can change
